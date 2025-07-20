d3bb8707a419c5c53d3f8fe9d0f1ef56d0f8e3e6
Sachin Goyal <114551556+Sachingoyal46@users.noreply.github.com>
2025-07-20 13:11:17 +0000

    data types

--- javascript_basic/test.js
+++ javascript_basic/test.js
@@ -1,2 +1,20 @@
+// const acc_id =1313
+// let acc_emaoil = "sdss"
+// var acc_emaoils ="12345" // having a scope problem  /*dfffff*/
+// let sachin 
 
-console.log("sachin g");
+// accountcity = "jaipur"
+// console.table([accountcity , acc_emaoil , acc_emaoils , sachin ])
+
+
+
+"use strict"; // treat all js code as new version
+
+// alert("maths ka dar hai"); // we are useing nide js not browser
+// null is a standalone value
+// undefined means not qassign value
+// symbol ---- unique
+
+// type of null is obejct 
+
+// type of undefined is undefined 
